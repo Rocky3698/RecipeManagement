c52cfcf1476c2b38f453cfd38d6f21669f8cd8f5
Rocky Chowdhury <rocky20809@gmail.com>
2024-05-30 22:24:34 +0600

    new

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from .models import Recipes 
+from .serializers import RecipeSerializer
 # Create your views here.
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Recipes.objects.all()
+    serializer_class = RecipeSerializer
